cf4a6cd5532c39732cc07b94771d68e27739f902
patelsahilua92 <l38546418@student.ua92.ac.uk>
2021-11-29 11:22:48 +0000

    update

---
+++ Form outcome.php
@@ -0,0 +1,32 @@
+<?php
+define("DB_NAME", "dynamite");
+define("DB_USER", "admin");
+define("DB_PASSWORD", "password");
+define("DB_HOST", "localhost");
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if (!$connection) {
+    die('Could not connect:'.mysqli_error());
+}
+else {
+    echo "It worked!";
+}
+$db_selected = mysqli_select_db($connection, DB_NAME);
+if (!$db_selected){
+    die('Can\'t use' .DB_NAME.': ' .mysqli_error($connection));
+}
+$name = $_POST["name"]; 
+$phone = $_POST["ContactNumber"];
+$email = $_POST["email"];
+$dateofbirth = $_POST["DateofBirth"];
+
+$sql = "INSERT INTO form(name, email, number, dateofbirth) VALUES ('$name', '$email', $phone, '$dateofbirth')";
+if(!mysqli_query($connection, $sql)){
+    die("Error:".mysqli_error($connection));
+}
+else{
+    echo "Data Inserted";
+}
+mysqli_close($connection);
+
+?>
+
---
+++ Form.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <title>MY first PHP</title>
+</head>
+<body>
+    
+    <center><h1>The PHP Form</h1></center>                                                                                                                  
+</body>
+<body>  
+<form action="Form_outcome.php" method ="POST">
+<center>Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Contact Number: <input type="integer" name="ContactNumber"><br>
+Date Of Birth: <input type="date" name="DateofBirth"><br>
+<input type="submit"></center>
+
+</form>
+</body>
+</html>
