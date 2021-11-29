0aff45b2bbd7debb8cc0025b3540160bf1fe06cb
patelsahilua92 <l38546418@student.ua92.ac.uk>
2021-11-29 11:37:29 +0000

    Update Form_outcome.php

--- Form_outcome.php
+++ Form_outcome.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_NAME", "dynamite");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
+define("DB_USER", "admin");
+define("DB_PASSWORD", "password");
 define("DB_HOST", "localhost");
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
@@ -10,7 +10,7 @@ if (!$connection) {
     die('Could not connect:'.mysqli_error());
 }
 else {
-    echo "It worked!";
+    echo "It worked!"."<br>";
 }
 $db_selected = mysqli_select_db($connection, DB_NAME);
 if (!$db_selected){
