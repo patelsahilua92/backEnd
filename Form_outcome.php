3460bcf29ec9d4926a78113f9ce0e9e15c893a84
patelsahilua92 <l38546418@student.ua92.ac.uk>
2021-11-29 11:35:03 +0000

    Update Form_outcome.php

--- Form_outcome.php
+++ Form_outcome.php
@@ -4,6 +4,8 @@ define("DB_USER", "root");
 define("DB_PASSWORD", "");
 define("DB_HOST", "localhost");
 
+echo "here";
+
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 if (!$connection) {
