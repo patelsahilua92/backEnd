fb952bd5c75794a3503a24749b538a3581cdf6fc
patelsahilua92 <l38546418@student.ua92.ac.uk>
2021-11-30 11:43:18 +0000

    update

---
+++ Astronaut_outcome.php
@@ -0,0 +1,30 @@
+<?php
+define("DB_NAME", "Dynamite");
+define("DB_USER", "admin");
+define("DB_PASSWORD", "password");
+define("DB_HOST", "localhost");
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+
+if (!$connection) {
+    die('Could not connect:'.mysqli_error());
+}
+else {
+    echo "It worked!"."<br>";
+}
+$db_selected = mysqli_select_db($connection, DB_NAME);
+if (!$db_selected){
+    die('Can\'t use ' .DB_NAME.': ' .mysqli_error($connection));
+}
+$astronaut_id = $_POST["astronaut_id"];
+$name = $_POST["name"]; 
+$no_missions = $_POST["no_missions"];
+
+$sql = "INSERT INTO astronaut(astronaut_id, name, no_mission) VALUES ('$astronaut_id', '$name', '$no_mission')";
+if(!mysqli_query($connection, $sql)){
+    die("Error:".mysqli_error($connection));
+}
+else{
+    echo "Data Inserted";
+}
+mysqli_close($connection);
