191902cb2fee3aeea8c93e1ea6bbfe51079ab938
patelsahilua92 <l38546418@student.ua92.ac.uk>
2021-11-30 11:53:57 +0000

    update

--- Astronaut.php
+++ Astronaut.php
@@ -13,7 +13,7 @@
     <center><h1>The Astronaut</h1></center>                                                                                                                  
 </body>
 <body>  
-<form action="Form_outcome.php" method ="POST">
+<form action="Astronaut_outcome.php" method ="POST">
 <center>astronaut_id: <input type="integer" name="astronaut_id"><br>
 name: <input type="text" name="name"><br>
 no_missions: <input type="integer" name="no_missions"><br>
