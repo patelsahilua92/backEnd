65491f2d500ed6b2b9e42fb71007e2c1a49ad853
patelsahilua92 <l38546418@student.ua92.ac.uk>
2021-12-03 10:35:17 +0000

    update

---
+++ targets_outcome.php
@@ -0,0 +1,34 @@
+<?php
+define("DB_NAME", "Dynamite");
+define("DB_USER", "admin");
+define("DB_PASSWORD", "password");
+define("DB_HOST", "localhost");
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+
+if (!$connection) {
+    die('Could not connect:'.mysqli_error());
+}
+else {
+    echo "It worked!"."<br>";
+}
+$db_selected = mysqli_select_db($connection, DB_NAME);
+if (!$db_selected){
+    die('Can\'t use ' .DB_NAME.': ' .mysqli_error($connection));
+}
+$id = $_POST["id"];
+$name = $_POST["name"]; 
+$first_missions = $_POST["first_missions"];
+$type = $_POST["type"];
+$no_mission = $_POST["no_mission"];
+
+$sql = "INSERT INTO targets(id, name, first_mission, type, no_missions) VALUES ('$id', '$name', '$first_mission', '$type', '$no_missions')";
+if(!mysqli_query($connection, $sql)){
+    die("Error:".mysqli_error($connection));
+}
+else{
+    echo "Data Inserted";
+}
+mysqli_close($connection);
+
+?>
