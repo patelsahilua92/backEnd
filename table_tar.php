5bf5a7dfc1f82492fc25b0778c2d5a8c54bce3d8
patelsahilua92 <l38546418@student.ua92.ac.uk>
2021-12-07 10:42:31 +0000

    update

--- table_tar.php
+++ table_tar.php
@@ -13,7 +13,7 @@
             <th>no_mission</th>
 </tr>
 <?php
-$conn = mysqli_connect("Dynamite","admin","password","localhost");
+$conn = mysqli_connect("localhost","admin","password","Dynamite");
 if ($conn-> connect_error) {
     die("Connection failed:". $conn-> connect_error);
 }
@@ -23,8 +23,8 @@ $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) { 
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row[
-            "first_mission"] . "</td><td>" . $row["type"] . "</td><td>" . $row["no_mission"] . "</td></tr>";
+        echo "<tr><td>" . $row["name"] . "</td><td>" . $row[ "first_mission"] . "</td><td>" . $row["type"] . 
+        "</td><td>" . $row["no_mission"] . "</td></tr>";
         }
 }
 else{
