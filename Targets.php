fe1ba0e5353fac53d7283ac71a0fda64ccee9c24
patelsahilua92 <l38546418@student.ua92.ac.uk>
2021-12-07 11:45:48 +0000

    up

--- Targets.php
+++ Targets.php
@@ -3,6 +3,8 @@
     <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     
   </head>
   </head>
@@ -49,12 +51,14 @@
           background-color: yellowgreen;
         }
         </style>
-    
-    <center><h1>The Targets</h1></center>                                                                                                                  
+    <div class="w3-text-red">
+    <center><h1>The Targets</h1></center>  
+      </div>                                                                                                                
 </body>
 <body background="sky 2.jfif">  
 <form action="targets_outcome.php" method ="POST">
 <center>
+<div class="w3-text-red">
 name: <input type="text" name="name"><br><br>
 first_mission: <input type="date" name="first_mission"><br><br>
 type: <input type="text" name="type"><br><br>
@@ -62,6 +66,7 @@ no_mission: <input type="number" name="no_mission"><br><br>
 <input type="submit">
 <br>
 </center>
+      </div>
 
 
 </form>
