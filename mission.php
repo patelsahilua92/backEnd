b663d58ff610c1c4a1a27e46a840f15effb354df
patelsahilua92 <l38546418@student.ua92.ac.uk>
2021-12-07 11:24:42 +0000

    update

--- mission.php
+++ mission.php
@@ -64,6 +64,6 @@ target_id: <input type="text" name="target_id"><br>
 <input type="submit"></center>
 
 </form>
-<a href="table_mis.php"><button>Show Table</button></a></center>
+<center><a href="table_mis.php"><button>Show Table</button></a></center>
 </body>
 </html>
