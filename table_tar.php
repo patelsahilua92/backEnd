3dee49d3a3b52346904f6f8bffc386520e123ed2
patelsahilua92 <l38546418@student.ua92.ac.uk>
2021-12-09 11:09:08 +0000

    up

--- table_tar.php
+++ table_tar.php
@@ -14,41 +14,8 @@
   </head>
   </head>
   <!-- this code is for nav-bar-->
-  <center><h1 style="background-color:DodgerBlue;">Welcome to Dynamite forms</h1></center>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link " aria-current="page" href="index.html">Home</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link" href="Targets.php">Target Form</a>
-           </li>
-           <li class="nav-item">
-          <a class="nav-link" href="mission.php">Mission Form</a>
-         
-           </li>
-          <li class="nav-item">
-            <a class="nav-link active" href="Astronaut.php">Astronaut Form</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="Attends.php">Attends Form</a>
-             </li>
-          
-           
-        </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
+  <center><h1 style="background-color:DodgerBlue;">Welcome to Target table</h1></center>
+  
   <!-- this is a title code-->
         <title>MY first PHP</title>
 </head>
