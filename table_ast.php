660bc5275016addf92de7e10b280347e7f9dc38c
patelsahilua92 <l38546418@student.ua92.ac.uk>
2021-12-09 11:16:02 +0000

    up

--- table_ast.php
+++ table_ast.php
@@ -5,6 +5,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    
+  </head>
+  </head>
+  <!-- this code is for nav-bar-->
+  <center><h1 style="background-color:DodgerBlue;">Welcome to Astronaut table</h1></center>
+  
+  <!-- this is a title code-->
+        <title>MY first PHP</title>
+</head>
 <body>
     <!-- this is a table code-->
 <table class="w3-table-all w3-hoverable">
@@ -35,5 +49,6 @@ else{
 }
 ?>
 </table>
+<center><a href="Astronaut.php"><button>Return</button></a></center>
 </body>
 </html>
