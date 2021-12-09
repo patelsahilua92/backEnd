cdaf6c281812cf9c3d7f4ef31c1297f36ff5508f
patelsahilua92 <l38546418@student.ua92.ac.uk>
2021-12-09 09:05:41 +0000

    update

--- table_ast.php
+++ table_ast.php
@@ -2,12 +2,16 @@
 <html>
     <head>
         <title>Table with database</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 <body>
-    <table>
-        <tr>
+<table class="w3-table-all w3-hoverable">
+    <thead>
+    <tr class="w3-light-grey">
             <th>name</th>
             <th>no_missions</th>
+</thead>
             
             
 </tr>
